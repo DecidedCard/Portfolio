75858ba794c8bb6fa0a15cdf1e7336fc6c607307
heajun jeong <heajun35@gmail.com>
2024-09-20 20:28:05 +0900

    [Feat] - tailwind fontSize setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,52 +9,52 @@ const config: Config = {
   theme: {
     extend: {
       fontSize: {
+        "headingR/32px": [
+          "32px",
+          { fontWeight: 400, lineHeight: "135%", letterSpacing: "-0.02em" },
+        ],
         "headingR/28px": [
           "28px",
           { fontWeight: 400, lineHeight: "135%", letterSpacing: "-0.02em" },
         ],
-        "headingR/24px": [
-          "24px",
-          { fontWeight: 400, lineHeight: "135%", letterSpacing: "-0.02em" },
+        "headingM/32px": [
+          "32px",
+          { fontWeight: 500, lineHeight: "135%", letterSpacing: "-0.02em" },
         ],
         "headingM/28px": [
           "28px",
           { fontWeight: 500, lineHeight: "135%", letterSpacing: "-0.02em" },
         ],
-        "headingM/24px": [
-          "24px",
-          { fontWeight: 500, lineHeight: "135%", letterSpacing: "-0.02em" },
+        "headingB/32px": [
+          "32px",
+          { fontWeight: 600, lineHeight: "135%", letterSpacing: "-0.02em" },
         ],
         "headingB/28px": [
           "28px",
           { fontWeight: 600, lineHeight: "135%", letterSpacing: "-0.02em" },
         ],
-        "headingB/24px": [
-          "24px",
-          { fontWeight: 600, lineHeight: "135%", letterSpacing: "-0.02em" },
+        "bodyR/20px": [
+          "20px",
+          { fontWeight: 400, lineHeight: "150%", letterSpacing: "0em" },
         ],
         "bodyR/18px": [
           "18px",
           { fontWeight: 400, lineHeight: "150%", letterSpacing: "0em" },
         ],
-        "bodyR/16px": [
-          "16px",
-          { fontWeight: 400, lineHeight: "150%", letterSpacing: "0em" },
+        "bodyM/20x": [
+          "20px",
+          { fontWeight: 500, lineHeight: "150%", letterSpacing: "0em" },
         ],
-        "bodyM/18x": [
+        "bodyM/18px": [
           "18px",
           { fontWeight: 500, lineHeight: "150%", letterSpacing: "0em" },
         ],
-        "bodyM/16px": [
-          "16px",
-          { fontWeight: 500, lineHeight: "150%", letterSpacing: "0em" },
+        "bodyB/20px": [
+          "20px",
+          { fontWeight: 600, lineHeight: "150%", letterSpacing: "0em" },
         ],
         "bodyB/18px": [
           "18px",
-          { fontWeight: 600, lineHeight: "150%", letterSpacing: "0em" },
-        ],
-        "bodyB/16px": [
-          "16px",
           { fontWeight: 600, lineHeight: "150%", letterSpacing: "0em" },
         ],
       },
