3e2ed0817afdff865e2ef78a233bae0770b96013
heajun jeong <heajun35@gmail.com>
2024-09-20 20:22:42 +0900

    [Feat] - header 작성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full h-24">
-      <h1>portfolio</h1>
+    <header className="flex justify-between p-3 w-full h-20 border-b border-solid border-black">
+      <div className="flex justify-between items-center mx-auto min-w-[1440px] max-w-[1440px]">
+        <h1 className="text-headingB/28px">portfolio</h1>
+        <div className="flex gap-3 text-headingB/24px">
+          <p>About Me</p>
+          <hr className="mx-1 w-[1px] h-8 bg-black" />
+          <p>Skill</p>
+          <hr className="mx-1 w-[1px] h-8 bg-black" />
+          <p>Archive</p>
+          <hr className="mx-1 w-[1px] h-8 bg-black" />
+          <p>Project</p>
+          <hr className="mx-1 w-[1px] h-8 bg-black" />
+          <p>History</p>
+        </div>
+      </div>
     </header>
   );
 };
