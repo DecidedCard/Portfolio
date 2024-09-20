a7fdb1c988cd8fb2feb29466b8efcaf805202797
heajun jeong <heajun35@gmail.com>
2024-09-20 19:57:34 +0900

    [Feat] - nextjs setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const pretendardSans = localFont({
+  src: "../../public/font/PretendardVariable.woff2",
+  variable: "--font-pretendard-sans",
+  weight: "45 920",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${pretendardSans.variable} antialiased`}>
         {children}
       </body>
     </html>
