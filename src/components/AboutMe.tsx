fd4a727b38fa75c5ce72d28f52b571008778fe65
heajun jeong <heajun35@gmail.com>
2024-09-21 21:32:31 +0900

    [Feat] - AboutMe 내용 수정

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,14 +6,27 @@ const AboutMe = () => {
       <div className="flex flex-col items-center gap-10 py-16 w-[1440px]">
         <h2 className="text-headingB/32px">About Me</h2>
         <div className="flex gap-24 text-bodyM/20px">
-          <div className="flex flex-col gap-5">
-            <p>이름: 정해준</p>
-            <p>주소지: 충청북도 청주시</p>
-            <p>이메일: heajun35@gmail.com</p>
+          <div className="flex justify-between gap-5 w-96">
+            <div className="flex flex-col gap-5">
+              <p>이름</p>
+              <p>주소지</p>
+              <p>이메일</p>
+            </div>
+            <div className="flex flex-col gap-5">
+              <p>정해준</p>
+              <p>충청북도 청주시</p>
+              <p>heajun35@gmail.com</p>
+            </div>
           </div>
-          <div className="flex flex-col gap-5">
-            <p>생년월일: 02.12.07</p>
-            <p>연락처: +82 10-2433-3987</p>
+          <div className="flex justify-between gap-5 w-96">
+            <div className="flex flex-col gap-5">
+              <p>생년월일</p>
+              <p>연락처</p>
+            </div>
+            <div className="flex flex-col gap-5">
+              <p>02.12.07</p>
+              <p>010-2433-3987</p>
+            </div>
           </div>
         </div>
       </div>
