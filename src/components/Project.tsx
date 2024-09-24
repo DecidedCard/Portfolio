84ccbd27f89470136622a706dd080d391860ff91
heajun jeong <heajun35@gmail.com>
2024-09-24 17:19:32 +0900

    [Feat] -프로젝트 정리

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,52 +3,93 @@ import React from "react";
 
 const Project = () => {
   return (
-    <div className="flex justify-center items-center p-20 bg-gray-600 text-white">
+    <div className="flex justify-center items-center p-20 bg-primary-200 text-text-200">
       <div className="flex flex-col gap-4 mx-auto w-[1440px]">
-        <h2 className="text-headingB/28px text-center">Project</h2>
-        <hr className="w-full h-[1px] border-0 bg-white" />
-        <div className="flex flex-col justify-center gap-3 mx-auto p-4 w-[1000px] bg-white text-black rounded-xl">
-          <p className="text-bodyB/20px">Porifo</p>
-          <span className="text-bodyR/18px text-gray-400">
+        <h2 className="text-headingB/28px text-center">PROJECT</h2>
+        <hr className="w-full border-text-200" />
+        <div className="flex flex-col justify-center gap-3 mx-auto p-4 w-[1000px] bg-accent-200 text-black rounded-xl">
+          <p className="text-bodyB/20px text-center">Porifo</p>
+          <span className="text-bodyR/18px text-gray-400 text-center">
             2024.04 ~ 2024.05
           </span>
           <hr className="border-black" />
-          <div className="flex gap-3">
+          <div className="flex gap-5">
             <div className="w-[400px] h-[500px] border border-solid border-black">
               이미지 들어갈곳
             </div>
-            <div className="flex flex-col gap-5 text-bodyM/20px">
-              <p>
-                porifo라는 이름의 이력서를 템플릿 기반으로 작성할 수 있고,
-                피드백을 받을 수 있는 프로젝트입니다.
-              </p>
-
-              <p>
-                Next.js를 처음으로 제대로 사용하여 어떻게 사용해야 되는지 알게
-                되는 계기가 되었습니다.
-              </p>
-
-              <p>
-                supabase를 사용하여 로그인 및 sql DB를 접하게 되는 계기가
-                되었습니다.
-              </p>
-
-              <Link
-                href={"https://github.com/DecidedCard/Porifo"}
-                target="blank"
-                className="p-4 w-fit bg-black rounded-md text-white"
-              >
-                자세히 보기
-              </Link>
-
-              <hr className="border-black" />
-
-              <div className="text-bodyR/18px">
-                <li className="flex gap-5 p-1">
-                  주요기능: 템플릿을 기반으로 이력서를 작성할 수 있고, 작성한
-                  이력서를 공유할 수 있습니다.
-                </li>
-              </div>
+            <hr className="my-auto w-[1px] h-[500px] border-0 bg-black" />
+            <div className="flex flex-col gap-3 text-bodyM/18px w-full">
+              <ol className="flex justify-between w-full">
+                <span className="w-[120px] text-end whitespace-nowrap">
+                  프로젝트 소개:
+                </span>
+                <span className="w-[500px]">
+                  porifo는 포트폴리오를 간단히 작성하고, 유저들끼리 피드백을
+                  받을 수 있는 사이트 입니다.
+                </span>
+              </ol>
+              <ol className="flex justify-between">
+                <span className="w-[120px] text-end whitespace-nowrap">
+                  기술 스택:
+                </span>
+                <span className="w-[500px]">
+                  Next.js, TypeScript, tailwindcss, zustand, Tanstack Query,
+                  supabase
+                </span>
+              </ol>
+              <ol className="flex justify-between">
+                <span className="w-[120px] text-end whitespace-nowrap">
+                  팀 구성:
+                </span>
+                <span className="w-[500px]">프론트엔드: 4명, 디자인: 1명</span>
+              </ol>
+              <ol className="flex justify-between">
+                <span className="w-[120px] text-end whitespace-nowrap">
+                  기능 구현:
+                </span>
+                <span className="flex flex-col gap-3 w-[500px]">
+                  <p>
+                    supabase DB와 로컬스토리지를 사용하여 로그인 유저와 비로그인
+                    유저를 구분하여 저장하도록 구현.
+                  </p>
+                  <p>작성한 데이터를 가져와서 다시 수정할 수 있도록 구현.</p>
+                  <p>
+                    route handler를 사용하여 지정한 요청만 수행할 수 있도록
+                    구현.
+                  </p>
+                  <p>
+                    로그인을 server를 활용할 수 있게 구현하여 보안성을 향상.
+                  </p>
+                </span>
+              </ol>
+              <ol className="flex justify-between">
+                <span className="w-[120px] text-end whitespace-nowrap">
+                  배포 링크:
+                </span>
+                <span className="w-[500px]">
+                  <Link
+                    href={"https://www.porifo.com/"}
+                    target="blank"
+                    className="cursor-pointer underline"
+                  >
+                    https://www.porifo.com/
+                  </Link>
+                </span>
+              </ol>
+              <ol className="flex justify-between">
+                <span className="w-[120px] text-end whitespace-nowrap">
+                  Github 링크:
+                </span>
+                <span className="w-[500px]">
+                  <Link
+                    href={"https://github.com/DecidedCard/Porifo"}
+                    target="blank"
+                    className="cursor-pointer underline"
+                  >
+                    https://github.com/DecidedCard/Porifo
+                  </Link>
+                </span>
+              </ol>
             </div>
           </div>
         </div>
