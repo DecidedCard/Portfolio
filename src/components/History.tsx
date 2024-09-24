87ff62baba51d56ef903e898d1981a158b94d4ce
heajun jeong <heajun35@gmail.com>
2024-09-24 20:23:35 +0900

    [Style] - css 수정

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -14,7 +14,7 @@ const History = () => {
                 <p className="text-headingB/28px">
                   스파르타코딩클럼/내일배움캠프 React-4기
                 </p>
-                <p className="text-gray-500">2024.01 ~ 2024.05</p>
+                <p className="text-gray-400">2024.01 ~ 2024.05</p>
                 <p>
                   약 4개월간 javascript와 React를 배우고 5개정도의 팀프로젝트를
                   진행하는 과정에서 팀원들과의 소통의 중요성을 알게 되었습니다.
@@ -24,7 +24,7 @@ const History = () => {
             <div className="flex justify-center gap-5 w-[550px] text-bodyM/20px">
               <div className="w-fit">
                 <p className="text-headingB/28px">실버링</p>
-                <p className="text-gray-500">인턴/2024.08 ~ 2024.08</p>
+                <p className="text-gray-400">인턴/2024.08 ~ 2024.08</p>
                 <p>
                   크레딧가드라는 이름의 부동산 관리 페이지의 mvp를
                   제작하였습니다.
