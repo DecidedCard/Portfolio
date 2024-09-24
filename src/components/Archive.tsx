2b9c15c6d0533bb2da949a10e08c1012ae771f5c
heajun jeong <heajun35@gmail.com>
2024-09-24 15:57:12 +0900

    [Feat] - archive 작성

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -1,31 +1,47 @@
+import Image from "next/image";
 import Link from "next/link";
+
+import github from "@/../public/assets/skillImage/github.svg";
+import velog from "@/../public/assets/skillImage/velog.jpg";
 
 const Archive = () => {
   return (
-    <div className="flex justify-center items-center py-20 bg-gray-500 text-white">
+    <div className="flex justify-center items-center py-20 bg-primary-100 text-text-200">
       <div className="flex flex-col gap-5 mx-auto w-[1440px]">
         <h2 className="text-headingB/32px text-center">ARCHIVING</h2>
-        <hr className="w-full h-[1px] border-0 bg-white" />
+        <hr className="w-full border-text-200" />
         <div className="flex justify-center gap-10">
           <Link
             href={"https://github.com/DecidedCard"}
             target="blank"
-            className="p-10 bg-white text-black rounded-xl"
+            className="flex flex-col gap-3 p-10 w-96 bg-accent-200 text-black rounded-xl text-bodyR/18px duration-300 hover:scale-105"
           >
-            <p className="text-headingB/28px">github</p>
-            <p>소스코드 저장소 입니다.</p>
-            <li>공부하면서 작성했던 소스코드</li>
-            <li>참여했던 프로젝트 소스코드</li>
+            <p className="flex items-center gap-5 text-headingB/28px">
+              <Image src={github} alt="github" className="w-10" />
+              Github
+            </p>
+            <p className="text-bodyM/20px">소스코드 저장소 입니다.</p>
+            <div className="-indent-6 ml-6">
+              <li>공부하면서 작성했던 소스코드</li>
+              <li>참여했던 프로젝트 소스코드</li>
+            </div>
           </Link>
           <Link
             href={"https://velog.io/@decidedcard"}
             target="blank"
-            className="p-10 bg-white text-black rounded-xl"
+            className="flex flex-col gap-3 p-10 w-96 bg-accent-200 text-black rounded-xl text-bodyR/18px duration-300 hover:scale-105"
           >
-            <p className="text-headingB/28px">blog</p>
-            <p>공부했던 기록들을 정리한 블로그입니다.</p>
-            <li>공부하였던 것들을 정리하는 곳</li>
-            <li>개발하면서 느꼈던 것들을 정히한 곳</li>
+            <p className="flex items-center gap-5 text-headingB/28px">
+              <Image src={velog} alt="velog" className="w-10" />
+              Blog
+            </p>
+            <p className="text-bodyM/20px">
+              공부했던 기록들을 정리한 블로그입니다.
+            </p>
+            <div className="-indent-6 ml-6">
+              <li>공부하였던 것들을 정리하는 곳</li>
+              <li>개발하면서 느꼈던 것들을 정히한 곳</li>
+            </div>
           </Link>
         </div>
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,26 +16,26 @@ const Header = ({
   onScrollToSkill,
 }: Props) => {
   return (
-    <header className="sticky top-0 flex justify-between p-3 w-full h-20 border-b border-solid border-accent-200">
+    <header className="sticky top-0 z-10 flex justify-between p-3 w-full h-20 bg-bg-300">
       <div className="flex justify-between items-center mx-auto min-w-[1440px] max-w-[1440px] text-text-200">
         <h1 className="text-headingB/32px">portfolio</h1>
         <nav className="flex gap-3 text-headingB/28px">
           <ol className="cursor-pointer" onClick={onScrollToAboutMe}>
             About Me
           </ol>
-          <hr className="mx-1 w-[1px] h-8 border-0 bg-black" />
+          <hr className="mx-1 w-[1px] h-8 border-0 bg-text-200" />
           <ol className="cursor-pointer" onClick={onScrollToSkill}>
             Skill
           </ol>
-          <hr className="mx-1 w-[1px] h-8 border-0 bg-black" />
+          <hr className="mx-1 w-[1px] h-8 border-0 bg-text-200" />
           <ol className="cursor-pointer" onClick={onScrollToArchive}>
             Archive
           </ol>
-          <hr className="mx-1 w-[1px] h-8 border-0 bg-black" />
+          <hr className="mx-1 w-[1px] h-8 border-0 bg-text-200" />
           <ol className="cursor-pointer" onClick={onScrollToProject}>
             Project
           </ol>
-          <hr className="mx-1 w-[1px] h-8 border-0 bg-black" />
+          <hr className="mx-1 w-[1px] h-8 border-0 bg-text-200" />
           <ol className="cursor-pointer" onClick={onScrollToHistory}>
             History
           </ol>
