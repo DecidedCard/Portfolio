26eb6aa600d63a35aa61a6f3588a2706b9a7e516
heajun jeong <heajun35@gmail.com>
2024-10-25 16:08:27 +0900

    [Feat] - project 컴포넌트 분리 및  내용 수정

---
+++ src/components/project/Porifo.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import Link from "next/link";
+
+import Slider from "./Slider";
+import MarkdownModal from "../modal/MarkdownModal";
+
+import useModalToggle from "@/hooks/useModalToggle";
+
+const Porifo = () => {
+  const { isModal, onClickIsModalToggleHandler } = useModalToggle();
+
+  return (
+    <div className="flex flex-col justify-center gap-3 mx-auto p-4 w-[1000px] bg-accent-200 text-black rounded-xl">
+      <p className="text-body/26px text-center">Porifo</p>
+      <span className="text-body/18px text-gray-400 text-center">
+        2024.04 ~ 2024.05
+      </span>
+      <hr className="border-black" />
+      <div className="relative flex gap-5">
+        <div className="flex w-[400px] h-[500px]">
+          <Slider />
+        </div>
+        <hr className="my-auto w-[1px] h-[500px] border-0 bg-black" />
+        <div className="flex flex-col gap-3 text-body/18px w-full">
+          <ol className="flex justify-between w-full">
+            <span className="w-[120px] text-end whitespace-nowrap">
+              프로젝트 소개:
+            </span>
+            <span className="w-[500px]">
+              porifo는 템플릿 기반으로 이력서를 작성 및 수정할 수 있고, 공유하여
+              유저들끼리 피드백을 받을 수 있는 사이트 입니다.
+            </span>
+          </ol>
+          <ol className="flex justify-between">
+            <span className="w-[120px] text-end whitespace-nowrap">
+              기술 스택:
+            </span>
+            <span className="w-[500px]">
+              Next.js, TypeScript, tailwindcss, zustand, Tanstack Query,
+              supabase
+            </span>
+          </ol>
+          <ol className="flex justify-between">
+            <span className="w-[120px] text-end whitespace-nowrap">
+              팀 구성:
+            </span>
+            <span className="w-[500px]">프론트엔드: 4명, 디자인: 1명</span>
+          </ol>
+          <ol className="flex justify-between">
+            <span className="w-[120px] text-end whitespace-nowrap">
+              기능 구현:
+            </span>
+            <span className="flex flex-col gap-3 w-[500px]">
+              <p> - Supabase를 사용하여 Oauth 및 이메일 로그인/회원가입 구현</p>
+              <p> - 이력서 작성 및 수정할 수 있는 작성 페이지 구현</p>
+              <p>
+                - 이력서 공유시 피드백을 받고 공유한 것을 확인할 수 있는
+                커뮤니티 페이지 구현
+              </p>
+            </span>
+          </ol>
+          <ol className="flex justify-between">
+            <span className="w-[120px] text-end whitespace-nowrap">
+              배포 링크:
+            </span>
+            <span className="w-[500px]">
+              <Link
+                href={"https://www.porifo.com/"}
+                target="blank"
+                className="cursor-pointer underline"
+              >
+                https://www.porifo.com/
+              </Link>
+            </span>
+          </ol>
+          <ol className="flex justify-between">
+            <span className="w-[120px] text-end whitespace-nowrap">
+              Github 링크:
+            </span>
+            <span className="w-[500px]">
+              <Link
+                href={"https://github.com/DecidedCard/Porifo"}
+                target="blank"
+                className="cursor-pointer underline"
+              >
+                https://github.com/DecidedCard/Porifo
+              </Link>
+            </span>
+          </ol>
+        </div>
+        <button
+          onClick={onClickIsModalToggleHandler}
+          className="absolute bottom-4 right-4 ml-4 p-2 w-fit border border-solid border-black rounded-lg"
+        >
+          README
+        </button>
+      </div>
+      {isModal && (
+        <MarkdownModal
+          isModal={isModal}
+          onClickIsModalToggleHandler={onClickIsModalToggleHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Porifo;
--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,115 +1,15 @@
 import React from "react";
-import Link from "next/link";
 
-import Slider from "./Slider";
-import MarkdownModal from "../modal/MarkdownModal";
-
-import useModalToggle from "@/hooks/useModalToggle";
+import Porifo from "./Porifo";
 
 const Project = () => {
-  const { isModal, onClickIsModalToggleHandler } = useModalToggle();
-
   return (
     <div className="py-20 bg-primary-200 text-text-200">
       <div className="flex flex-col gap-10 mx-auto w-[1440px]">
         <h2 className="text-title/40px text-center">PROJECT</h2>
         <hr className="w-full border-text-200" />
-        <div className="flex flex-col justify-center gap-3 mx-auto p-4 w-[1000px] bg-accent-200 text-black rounded-xl">
-          <p className="text-body/26px text-center">Porifo</p>
-          <span className="text-body/18px text-gray-400 text-center">
-            2024.04 ~ 2024.05
-          </span>
-          <hr className="border-black" />
-          <div className="flex gap-5">
-            <div className="flex w-[400px] h-[500px]">
-              <Slider />
-            </div>
-            <hr className="my-auto w-[1px] h-[500px] border-0 bg-black" />
-            <div className="flex flex-col gap-3 text-body/18px w-full">
-              <ol className="flex justify-between w-full">
-                <span className="w-[120px] text-end whitespace-nowrap">
-                  프로젝트 소개:
-                </span>
-                <span className="w-[500px]">
-                  porifo는 포트폴리오를 간단히 작성하고, 유저들끼리 피드백을
-                  받을 수 있는 사이트 입니다.
-                </span>
-              </ol>
-              <ol className="flex justify-between">
-                <span className="w-[120px] text-end whitespace-nowrap">
-                  기술 스택:
-                </span>
-                <span className="w-[500px]">
-                  Next.js, TypeScript, tailwindcss, zustand, Tanstack Query,
-                  supabase
-                </span>
-              </ol>
-              <ol className="flex justify-between">
-                <span className="w-[120px] text-end whitespace-nowrap">
-                  팀 구성:
-                </span>
-                <span className="w-[500px]">프론트엔드: 4명, 디자인: 1명</span>
-              </ol>
-              <ol className="flex justify-between">
-                <span className="w-[120px] text-end whitespace-nowrap">
-                  기능 구현:
-                </span>
-                <span className="flex flex-col gap-3 w-[500px]">
-                  <p>
-                    supabase DB와 로컬스토리지를 사용하여 로그인 유저와 비로그인
-                    유저를 구분하여 저장하도록 구현.
-                  </p>
-                  <p>이력서 CRUD 구현</p>
-                  <p>
-                    route handler를 사용하여 지정한 요청만 수행할 수 있도록
-                    구현.
-                  </p>
-                </span>
-              </ol>
-              <ol className="flex justify-between">
-                <span className="w-[120px] text-end whitespace-nowrap">
-                  배포 링크:
-                </span>
-                <span className="w-[500px]">
-                  <Link
-                    href={"https://www.porifo.com/"}
-                    target="blank"
-                    className="cursor-pointer underline"
-                  >
-                    https://www.porifo.com/
-                  </Link>
-                </span>
-              </ol>
-              <ol className="flex justify-between">
-                <span className="w-[120px] text-end whitespace-nowrap">
-                  Github 링크:
-                </span>
-                <span className="w-[500px]">
-                  <Link
-                    href={"https://github.com/DecidedCard/Porifo"}
-                    target="blank"
-                    className="cursor-pointer underline"
-                  >
-                    https://github.com/DecidedCard/Porifo
-                  </Link>
-                </span>
-              </ol>
-              <button
-                onClick={onClickIsModalToggleHandler}
-                className="ml-4 p-2 w-fit border border-solid border-black rounded-lg"
-              >
-                README
-              </button>
-            </div>
-          </div>
-        </div>
+        <Porifo />
       </div>
-      {isModal && (
-        <MarkdownModal
-          isModal={isModal}
-          onClickIsModalToggleHandler={onClickIsModalToggleHandler}
-        />
-      )}
     </div>
   );
 };
